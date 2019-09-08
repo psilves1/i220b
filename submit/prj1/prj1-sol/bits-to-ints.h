dbb506b31cb415802aeeba8407b3224e203b85ac
Paul Silvestri <psilves1@cs.binghamton.edu>
2019-09-08 15:18:03 -0400

    Finished the project

--- submit/prj1/prj1-sol/bits-to-ints.h
+++ submit/prj1/prj1-sol/bits-to-ints.h
@@ -1,13 +1,12 @@
-#include "bits-to-ints.h"
-#include "errors.h"
+#ifndef BITS_TO_INTS_H_
+#define BITS_TO_INTS_H_
 
-#include <assert.h>
-#include <ctype.h>
-#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-//@TODO: auxiliary definitions
+/** The type of a result and a format for printf()'ing it */
+typedef unsigned long long BitsValue;
+#define BITS_VALUE_FORMAT "ll"
 
 /** This function should be called with inFile set to an input FILE
  *  stream corresponding to a file named inName.  This FILE should
@@ -42,66 +41,7 @@
  *  in inFile, then a suitable error message should be printed and the
  *  function should return with *isEof set to true.
  */
+BitsValue bits_to_ints(FILE *inFile, const char *inName, int nBits,
+                       bool *isEof);
 
-int bitsToNum(char n0, char n1, char n2, char n3){
-
-  int returnVal = 0;  
-  
-  int v0 = n0 - '0';
-  int v1 = n1 - '0';
-  int v2 = n2 - '0';
-  int v3 = n3 - '0';
-  
-  returnVal += v0;
-  returnVal += v1 * 2;
-  returnVal += v2 * 4;
-  returnVal += v3 * 8;
- 
-  return returnVal;
-}
-
- 
-BitsValue
-bits_to_ints(FILE *inFile, const char *inName, int nBits, bool *isEof)
-{
-  //nBits value should make sense
-  assert(0 < nBits && nBits <= CHAR_BIT*sizeof(BitsValue));
-  BitsValue value = 0;
-  //@TODO
-
-  int valueHolder[nBits/4];
-  char bitHolder[8];
-  int valueHolderIndex = 0;
-  
-      for(int i = 0; i < 8; i++){
-
-      char c = fgetc(inFile);     
-
-      if(feof(inFile)){
-	*isEof = true;
-	break;
-      }
-      
-      if(isspace(c)){
-	i--;
-      }
-      else{
-	bitHolder[i] = c;
-      }
-      } 
-     
-    valueHolder[valueHolderIndex] = bitsToNum(bitHolder[4],bitHolder[5], bitHolder[6], bitHolder[7]);
-    valueHolderIndex++;
-    valueHolder[valueHolderIndex] = bitsToNum(bitHolder[0],bitHolder[1],bitHolder[2],bitHolder[3])*16;
-    valueHolderIndex++;
-    
-    // printf("%d \n" , valueHolder[0]);
-    //printf("%d \n" , valueHolder[1]);
- 
-    for(int i = valueHolderIndex; i > 0; i--){
-
-      value += valueHolder[i-1];
-    }
-	
-  return value;
-}
+#endif //ifndef BITS_TO_INTS_H_
